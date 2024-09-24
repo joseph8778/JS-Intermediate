a43043f52327e32e743da872e3d33987b6b7888e
Joseph <joseph8778@gmail.com>
2024-09-24 12:08:52 -0500

    clear

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-function totalFood(arr) {
-let sum = 0
-for (let i = 0; i < arr.length; ++i) {
-    sum = sum + arr[i]}
-    return sum
+function progressiveSum(num) {
+sum = 0
+for (let i = 1; i <= num; ++i) {
+sum = sum + i} //starts from 1, then goes to 2, then stops at 3 since 4 is greater than.
+return sum //After 4 loop is stopped, we return the new sum.
 }
 
 
 
 
-console.log(totalFood([5, 6, 3, 20]))
+console.log(progressiveSum(3))
