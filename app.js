83366fd00bb30b3f89e1067e3ae811270a84bea2
Joseph <joseph8778@gmail.com>
2024-09-24 14:28:37 -0500

    Get maximum num of array function added.

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
-function calcTime(time) {
-let seconds = time % 60 //Seconds
-let minutes = Math.floor(time / 60) //hours''
-
-if (minutes.toString().length === 1) {
-     minutes = '0' + minutes
+function getMax(arr) {
+let max = 0
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+        max = arr[i]
+    }
+}
+return max
 }
 
-if (seconds.toString().length === 1) {
-     seconds = '0' + seconds
-}
 
-
-return `${minutes}:${seconds}`
-}
-
-console.log(calcTime(606));
+console.log(getMax([5, 100, 0]))
