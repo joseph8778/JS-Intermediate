48e4c975db071930c25f40c11a41c2222bc815cb
Joseph <joseph8778@gmail.com>
2024-09-26 14:31:43 -0500

    convert to boolean function added

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
-function filterOutFalsy(arr) {
-    let truthyArr = [];
-    for (let i = 0; i < arr.length; i++) {
-        if (!!arr[i] === true) {
-            truthyArr.push(arr[i])
-        }
-    }
-    return truthyArr
+
+function convertToBoolean(arr) {
+return arr.map(elem => !!elem)
 }
 
-console.log(filterOutFalsy(['banana', 0, undefined, NaN, 1 ]))
+
+console.log(convertToBoolean([500, 0, "David", "", []]))
