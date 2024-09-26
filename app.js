eb1a022e50ffc7dd41cd7a9346ffa43f9b14d865
Joseph <joseph8778@gmail.com>
2024-09-26 13:17:55 -0500

    convert array to zero functions.

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
-function getMax(arr) {
-let max = 0
+//FOR LOOP
+
+function convertToZeros(arr) {
+let zero = []
 for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > max) {
-        max = arr[i]
-    }
+    zero.push(0)
 }
-return max
+return zero
 }
 
+console.log(convertToZeros([1, 2, 3, 4, 5]))
 
-console.log(getMax([5, 100, 0]))
+// MAP
+
+let arry = [1, 2, 3, 4, 5, 6, 6, 6]
+
+let convertedZeroArray = arry.map((element) => {
+    return element * 0
+})
+
+console.log(convertedZeroArray)
+
+//FILL
+
+function convertZeros(arg) {
+return new Array(arg.length).fill(0)
+}
+
+console.log(convertZeros([1, 2, 4, 6]))
