b2bd7d88cef98e9c1b5a890c24068ba82ff9f12b
Joseph <joseph8778@gmail.com>
2024-09-26 14:12:36 -0500

    filter falsy filter function

--- app.js
+++ app.js
@@ -1,21 +1,5 @@
-function removeApplesForLoop(forArr) {
-let noApples = []
-    for (let i = 0; i < forArr.length; i++) {
-   if (forArr[i] !== 'Apple') {
-    noApples.push(forArr[i])}
-    }
-return noApples
+function filterOutFalsy(arr) {
+    return arr.filter(elem => !!elem === true)
 }
 
-console.log(removeApplesForLoop(['Banana', 'Orange', 'Apple']))
-
-
-
-
-//Filter ARRAY FUNCTION
-
-function noNewApples(filterArray) {
-return filterArray.filter(elem => elem !== 'Apple')
-}
-
-console.log(noNewApples(['Banana', 'Orange', 'Apple', "tomato", 'sausage']))
+console.log(filterOutFalsy(['banana', NaN, undefined, null, false, 0, 'Nigga']))
