03ce71ae46406c792c4b83390275eca573a43a2e
Joseph <joseph8778@gmail.com>
2024-09-26 14:16:15 -0500

    filterOutfalsy for loop function

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 function filterOutFalsy(arr) {
-    return arr.filter(elem => !!elem === true)
+    let truthyArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (!!arr[i] === true) {
+            truthyArr.push(arr[i])
+        }
+    }
+    return truthyArr
 }
 
-console.log(filterOutFalsy(['banana', NaN, undefined, null, false, 0, 'Nigga']))
+console.log(filterOutFalsy(['banana', 0, undefined, NaN, 1 ]))
